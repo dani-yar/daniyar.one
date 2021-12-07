0e654d3ef419126ec3c0230b6bff707b2a4fa0ce
Daniyar Bogdanov <69664720+daniyarone@users.noreply.github.com>
2021-12-07 10:39:45 +0500

    Update code.js

--- js/code.js
+++ js/code.js
@@ -13,3 +13,9 @@ function mmenu_anim() {
         setTimeout("document.getElementById('mmenu3').style.marginLeft = '100px'; document.getElementById('mmenu3').style.opacity = '0'", 300);
     }
 }
+
+if (window.location.protocol!='http:') {}
+else {
+    window.location.href = "https://daniyar.one";
+}
+
